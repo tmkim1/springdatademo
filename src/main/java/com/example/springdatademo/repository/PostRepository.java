8502095723bf033fbb277623f3649bb34966b927
tmkim1 <xoalstjtm@gmail.com>
2022-03-12 20:26:50 +0900

    JPA Repository Test

--- src/main/java/com/example/springdatademo/repository/PostRepository.java
+++ src/main/java/com/example/springdatademo/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.example.springdatademo.repository;
 
 import com.example.springdatademo.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByTitleContains(String title, Pageable pageable);
 
+    long countByTitleContains(String spring);
 }
---
+++ src/test/java/com/example/springdatademo/repository/PostRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.example.springdatademo.repository;
+
+import com.example.springdatademo.entity.Post;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    @Rollback(value = false)
+    public void crudRepository() {
+        //Given
+        Post post = new Post();
+        post.setTitle("Hello Spring Boot Common");
+        assertThat(post.getId()).isNull();
+
+        //When
+        Post newPost = postRepository.save(post);
+
+        //Then
+        assertThat(newPost.getId()).isNotNull();
+
+        //When
+        List<Post> posts = postRepository.findAll();
+
+        //Then
+        assertThat(posts.size()).isEqualTo(1);
+        assertThat(posts).contains(newPost);
+
+        //When
+        Page<Post> page = postRepository.findAll(PageRequest.of(0, 10));
+        //Then
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+
+        //When
+        postRepository.findByTitleContains("Spring", PageRequest.of(0, 10));
+        //Then
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+
+        //When
+        long springCnt = postRepository.countByTitleContains("Spring");
+        assertThat(springCnt).isEqualTo(1);
+    }
+}
